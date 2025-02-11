eb425c3334f56b9a722799a7d67a516bc6e5ad12
Gabriel Aguiar <146899510+GabrielAguiarF@users.noreply.github.com>
2025-02-11 17:34:16 -0300

    rm: front-end

--- frontend/src/functions/normalizeTimeStamp.ts
+++
@@ -1,9 +0,0 @@
-export default function normalizeTimeStamp(isoString: string) {
-  let date = new Date(isoString);
-  let pad = (n: number) => (n < 10 ? "0" + n : n);
-  return `${pad(date.getDate())}/${pad(
-    date.getMonth() + 1
-  )}/${date.getFullYear()} ${pad(date.getHours())}:${pad(
-    date.getMinutes()
-  )}:${pad(date.getSeconds())}`;
-}
--- frontend/src/functions/removerPontuaction.ts
+++
@@ -1,3 +0,0 @@
-export function removePunctuation(value: string): string {
-   return value.replace(/[^\d]/g, "");
-}
